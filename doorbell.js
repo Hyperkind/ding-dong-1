71c09ebe1f7b0d332c417172fa7f51e138c688a3
Jon Ah-Nee <jonahnee@gmail.com>
2016-03-12 16:13:04 -1000

    added mailgun

--- doorbell.js
+++ doorbell.js
@@ -2,8 +2,11 @@ var Gpio = require('onoff').Gpio,
     led = new Gpio(14, 'out'),
     button = new Gpio(4, 'in', 'both');
 var spawn = require('child_process').spawn;
-
-
+var moment = require('moment');
+var filepath = path.join(__dirname, 'whosthere.jpg');
+var mailgun = require('mailgun-js')({apiKey: 'key-5cf681c09c48516a8e0b8b00a580969c', domain: 'sandbox08ea930e43c140d699bd4406076fd445.mailgun.org'});
+var CONFIG = require('./config.json');
+var user = CONFIG;
 
 module.exports = {
 
@@ -18,6 +21,22 @@ module.exports = {
         var cameraSh = spawn('sh', ['camera.sh'], {
           cwd: undefined,
           env: process.env
+        });
+
+        var data = {
+          from: 'Excited User <me@samles.mailgun.org>',
+          to: user.phone + '@mms.att.net',
+          subject: user.subject,
+          text: moment().format('MMMM Do YYYY, h:mm:ss a'),
+          attachment: filepath
+        };
+
+        mailgun.messages().send(data, function(error, body) {
+          if(error) {
+            console.log(error);
+          } else {
+            console.log(body);
+          }
         });
       }
       led.writeSync(value);
